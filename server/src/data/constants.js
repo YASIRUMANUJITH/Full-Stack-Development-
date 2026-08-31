fdd4aa9f1d28d073c763129ec4fe840e0fbed913
Yasiru Manujith <125721880+YASIRUMANUJITH@users.noreply.github.com>
2026-08-31 19:13:21 +0530

    feat : add more mock store

---
+++ server/src/data/constants.js
@@ -0,0 +1,9 @@
+export const PRIORITIES = ['high', 'medium', 'low']
+
+export const LABELS = ['design', 'api', 'docs', 'testing', 'deploy']
+
+export const BOARD_COLUMNS = [
+  { id: 'todo', title: 'To Do' },
+  { id: 'doing', title: 'Doing' },
+  { id: 'done', title: 'Done' },
+]
--- server/src/server.js
+++
@@ -1,6 +0,0 @@
-import app from './app.js'
-import config from './config.js'
-
-app.listen(config.port, () => {
-  console.log(`Server running on http://localhost:${config.port}`)
-})
